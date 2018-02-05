8526bf27aefdbb825b944c2041dee6371122930d
Fred Hsu <fredlhsu@gmail.com>
2018-02-05 14:57:35 -0800

    Updating with phoenix

--- oracle.py
+++ oracle.py
@@ -10,14 +10,18 @@ from flask_cors import CORS
 from jsonrpclib import Server
 
 # Use these for corporate
-# compartment_id = "ocid1.tenancy.oc1..aaaaaaaaawiselp4yjvnsjxr6cwabw2lvefolabds5tufevvbuojbf3s7kdq"
-# subnet_id = "ocid1.subnet.oc1.phx.aaaaaaaaursbyjzlzmff5a2otmlouuq3kiojckiovzqcsy4comnao3vizloa"
-# image_id = "ocid1.image.oc1.phx.aaaaaaaacv5sgh7zwvnjfzq72eltwtzkwrv4vhr2ashl6dq2gqnm3fe3a6ua"
+compartment_id = "ocid1.tenancy.oc1..aaaaaaaaawiselp4yjvnsjxr6cwabw2lvefolabds5tufevvbuojbf3s7kdq"
+subnet_id = "ocid1.subnet.oc1.phx.aaaaaaaaursbyjzlzmff5a2otmlouuq3kiojckiovzqcsy4comnao3vizloa"
+image_id = "ocid1.image.oc1.phx.aaaaaaaaajgwxitqioomenesux4jfcjwosx5b523x22tffqv3m2howjknjbq"
+profile_name = "DEFAULT"
+avail_domain = "qPIx:PHX-AD-1"
 
 # Use these for personal
-compartment_id = "ocid1.tenancy.oc1..aaaaaaaazco3xpwhyer3ayza5wl6uldqxulyqsdcmnvxykv4fr457n73c5rq"
-subnet_id = "ocid1.subnet.oc1.iad.aaaaaaaah3nz3q5szlpvuwiwdjer6pwzm6ur3mrnxgkt62yosiytemgreihq"
-image_id = "ocid1.image.oc1.iad.aaaaaaaacbgdflm66sxcdxriiatfrr27hb5wsy7hwg6wooocpsl7i3gld6ga"
+# compartment_id = "ocid1.tenancy.oc1..aaaaaaaazco3xpwhyer3ayza5wl6uldqxulyqsdcmnvxykv4fr457n73c5rq"
+# subnet_id = "ocid1.subnet.oc1.iad.aaaaaaaah3nz3q5szlpvuwiwdjer6pwzm6ur3mrnxgkt62yosiytemgreihq"
+# image_id = "ocid1.image.oc1.iad.aaaaaaaacbgdflm66sxcdxriiatfrr27hb5wsy7hwg6wooocpsl7i3gld6ga" # fredcustom
+# profile_name = "PERSONAL"
+# avail_domain = "DESs:US-ASHBURN-AD-1"
 
 try:
     _create_unverified_https_context = ssl._create_unverified_context
@@ -70,53 +74,73 @@ def setHostname(host_ip):
     hostname = "veos-oci-" + host_ip.replace('.','-')
     print("Setting hostname to " + hostname)
     switch = Server("https://cvpadmin:arista123@" + host_ip + "/command-api" )
-    response = switch.runCmds( 1, ["enable", 
-        "configure", 
-        "hostname " + hostname,
-        "ip name-server vrf default 8.8.8.8",
-        "interface loopback 0", 
-        "ip address " + host_ip + "/32", 
-        "no shutdown"])
+    success = False
+    try:
+        response = switch.runCmds( 1, ["enable", 
+            "configure", 
+            "hostname " + hostname,
+            "ip name-server vrf default 8.8.8.8",
+            "daemon TerminAttr" +
+	    "exec /usr/bin/TerminAttr -ingestgrpcurl=162.210.129.23:9910 -taillogs -ingestauth=key," +
+            "management api http-commands",
+            "protocol http",
+            "interface loopback 0", 
+            "ip address " + host_ip + "/32", 
+            "no shutdown"])
+        print("configuration successful")
+        success = true
+    except:
+        print("Did not connect, waiting 5 seconds")
+        sleep(5)
+        response = switch.runCmds( 1, ["enable", 
+            "configure", 
+            "hostname " + hostname,
+            "ip name-server vrf default 8.8.8.8",
+            "interface loopback 0", 
+            "ip address " + host_ip + "/32", 
+            "no shutdown"])
     return hostname
 
-def xcreate_instance(config, request):
-    compute = oci.core.compute_client.ComputeClient(config)
-    vnet = oci.core.virtual_network_client.VirtualNetworkClient(config)
-    source_details = oci.core.models.InstanceSourceViaImageDetails()
-    source_details.image_id = image_id
+# def xcreate_instance(config, request):
+#     compute = oci.core.compute_client.ComputeClient(config)
+#     vnet = oci.core.virtual_network_client.VirtualNetworkClient(config)
+#     source_details = oci.core.models.InstanceSourceViaImageDetails()
+#     source_details.image_id = image_id
 
-    create_vnic_details = oci.core.models.CreateVnicDetails()
-    create_vnic_details.assign_public_ip = True
-    create_vnic_details.subnet_id = subnet_id
+#     create_vnic_details = oci.core.models.CreateVnicDetails()
+#     create_vnic_details.assign_public_ip = True
+#     create_vnic_details.subnet_id = subnet_id
 
-    request = oci.core.models.LaunchInstanceDetails()
-    # request.availability_domain = "qPIx:PHX-AD-1"
-    request.availability_domain = "DESs:US-ASHBURN-AD-1"
-    request.compartment_id = compartment_id
-    request.create_vnic_details = create_vnic_details
-    request.shape = "VM.Standard1.2"
-    request.source_details = source_details
-    request.display_name = "veos-oci-demo"
+#     request = oci.core.models.LaunchInstanceDetails()
+#     # request.availability_domain = "qPIx:PHX-AD-1"
+#     request.availability_domain = "DESs:US-ASHBURN-AD-1"
+#     request.compartment_id = compartment_id
+#     request.create_vnic_details = create_vnic_details
+#     request.shape = "VM.Standard1.2"
+#     request.source_details = source_details
+#     request.display_name = "veos-oci-demo"
 
-    launch = compute.launch_instance(request)
-    print("## Launched Instance: ##")
-    new_instance = launch.data
-    print(new_instance)
-    sleep(30)
-    vnics = compute.list_vnic_attachments(compartment_id, instance_id = new_instance.id)
-    for v in vnics.data:
-        vnic = vnet.get_vnic(v.vnic_id)
-        print("private ip: " + vnic.data.private_ip)
-        print("public ip: " + vnic.data.public_ip)
-    sleep(120)
+#     launch = compute.launch_instance(request)
+#     print("## Launched Instance: ##")
+#     new_instance = launch.data
+#     print(new_instance)
+#     sleep(30)
+#     vnics = compute.list_vnic_attachments(compartment_id, instance_id = new_instance.id)
+#     for v in vnics.data:
+#         vnic = vnet.get_vnic(v.vnic_id)
+#         print("private ip: " + vnic.data.private_ip)
+#         print("public ip: " + vnic.data.public_ip)
+#     sleep(120)
 
 @app.route('/oci/api/v1.0/vrouter', methods=['POST'])
 def create_router():
     if request.method == 'POST':
-        config = oci.config.from_file(profile_name="PERSONAL")
+        # config = oci.config.from_file(profile_name="PERSONAL")
+        config = oci.config.from_file(profile_name=profile_name)
         print(request.json)
         req = oci.core.models.LaunchInstanceDetails()
-        req.availability_domain = request.json['availabilityDomain']
+        # req.availability_domain = request.json['availabilityDomain']
+        req.availability_domain = avail_domain
         req.compartment_id = compartment_id
         req.shape = request.json['shape']
         req.shape = "VM.Standard1.2"
@@ -125,5 +149,5 @@ def create_router():
         return jsonify({'task':'complete'}), 201
 
 if __name__ == '__main__':
-    print("Running server")
+    print("Running Oracle API server")
     app.run(debug=True)
